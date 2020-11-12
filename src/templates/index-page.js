931eaca2cb17ec06d9d5d750821e87f6a0902f9e
sbodrero <bodrero.sebastien@gmail.com>
2020-11-12 15:20:46 +0100

    Updated landing page structure

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -158,6 +158,7 @@ export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
+        title
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
@@ -165,36 +166,8 @@ export const pageQuery = graphql`
             }
           }
         }
-        title
-        subheading
-        main {
-            littleTitle
-            bigTitle
-            description
-            chiffre {
-                number
-                details
-            }
-        }
-        blocs {
-            carre {
-                image {
-                    childImageSharp {
-                    fluid(maxWidth: 240, quality: 64) {
-                      ...GatsbyImageSharpFluid
-                    }
-                  }
-                }
-                text
-            }
-        }
-        certifications {
-            image
-        }
-        values {
-            text
-        }
         heading
+        subheading
         mainpitch {
           title
           description
