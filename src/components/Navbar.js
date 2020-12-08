ed729bd4b1785fb6ead9c71d8ed296209150016d
sbodrero <bodrero.sebastien@gmail.com>
2020-12-08 14:11:06 +0100

    Added Home to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,6 +57,9 @@ const Navbar = class extends React.Component {
               className={`navbar-menu ${this.state.navBarActiveClass}`}
             >
               <div className="navbar-start has-text-centered">
+                <Link className="navbar-item" to="/">
+                  Accueil
+                </Link>
                 <Link className="navbar-item" to="/about">
                   About
                 </Link>
