ba01482ffe8df23b80c1092917a2c7812cddd27e
sbodrero <bodrero.sebastien@gmail.com>
2020-12-24 11:53:28 +0100

    Added amb page

---
+++ src/pages/amb/index.md
@@ -0,0 +1,11 @@
+---
+templateKey: 'amb-page'
+path: /amb
+title: Account Based Marketing
+image: /img/products-full-width.jpg
+subheading: Agence Inbound marketing pour les PMI
+inner_image: /img/products-full-width.jpg
+button:
+  text: Réservez votre entretien
+  url: /entretien
+---
--- src/templates/amb-page.js
+++ src/templates/amb-page.js
@@ -8,6 +8,7 @@ export const AmbPageTemplate = ({
     image,
     title,
     subheading,
+    button,
     inner_image,
 }) => (
     <div>
@@ -65,7 +66,7 @@ export const AmbPageTemplate = ({
                         <div className="column">
                             <div className="content">
                                 <div className="tile">
-                                    <h2 className="title has-text-white-ter">
+                                    <h2 className="title has-text-white-ter has-text-centered">
                                         Qu’est que l’Account Based Marketing (ABM)?
                                     </h2>
                                 </div>
@@ -81,34 +82,32 @@ export const AmbPageTemplate = ({
                     <div className="columns">
                         <div className="column">
                             <div className="content">
-                                <div className="tile">
-                                    <p>
-                                        <srong>L’Account Based Marketing</srong>
-                                        (ou «marketing des comptes stratégiques») consiste à mettre en place
-                                        des campagnes de marketing digitale B2B focalisées sur un nombre restreint de
-                                        prospects à forte valeurajoutée (grands comptes).
-                                    </p>
-                                    <p>
-                                        Cette approche très ciblée, permet d'élaborer des campagnes personnalisées
-                                        avec des entreprises susceptibles de se convertir en acheteurs.
-                                    </p>
-                                    <ul> Certaines pratiques de l’ABM sont cependant innovantes, en l’occurrence :
-                                        <li>
-                                            Des articles de blog et du marketing de contenus pour attirer les
-                                            personas en phase de prospection (ciblage seo)
-                                        </li>
-                                        <li>
-                                            Une expérience client / utilisateur on-site ultra-personnalisée
-                                        </li>
-                                        <li>
-                                            Des stratégies réseaux sociaux et du social selling
-                                        </li>
-                                        <li>
-                                            CRM et marketing automation pour faciliter la communication et la
-                                            collaboration des équipes de ventes et de marketing
-                                        </li>
-                                    </ul>
-                                </div>
+                                <p>
+                                    <srong>L’Account Based Marketing</srong>
+                                    (ou «marketing des comptes stratégiques») consiste à mettre en place
+                                    des campagnes de marketing digitale B2B focalisées sur un nombre restreint de
+                                    prospects à forte valeurajoutée (grands comptes).
+                                </p>
+                                <p>
+                                    Cette approche très ciblée, permet d'élaborer des campagnes personnalisées
+                                    avec des entreprises susceptibles de se convertir en acheteurs.
+                                </p>
+                                <ul> Certaines pratiques de l’ABM sont cependant innovantes, en l’occurrence :
+                                    <li>
+                                        Des articles de blog et du marketing de contenus pour attirer les
+                                        personas en phase de prospection (ciblage seo)
+                                    </li>
+                                    <li>
+                                        Une expérience client / utilisateur on-site ultra-personnalisée
+                                    </li>
+                                    <li>
+                                        Des stratégies réseaux sociaux et du social selling
+                                    </li>
+                                    <li>
+                                        CRM et marketing automation pour faciliter la communication et la
+                                        collaboration des équipes de ventes et de marketing
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
@@ -122,7 +121,7 @@ export const AmbPageTemplate = ({
                         <div className="column">
                             <div className="content">
                                 <div className="tile">
-                                    <h2 className="title has-text-white-ter">
+                                    <h2 className="title has-text-white-ter has-text-centered">
                                         Quel est la différence entre Inbound Marketing et ABM?
                                     </h2>
                                 </div>
@@ -138,36 +137,35 @@ export const AmbPageTemplate = ({
                     <div className="columns">
                         <div className="column">
                             <div className="content">
-                                <div className="tile">
-                                    <ul> L'inbound marketing et l'account-based marketing sont au premier abord quelque
-                                        peu différents :
-                                        <li>
-                                            L'<strong>inbound marketing</strong> a pour objectif de créer du contenu de qualité pour
-                                            attirer des prospects qualifiés.
-                                        </li>
-                                        <li>
-                                            L'<strong>accound-based marketing</strong>, lui, met l'accent sur les prospects ou comptes
-                                            existants de manière individuelle.
-                                        </li>
-                                    </ul>
-                                    <p>
-                                        Toutefois, ces deux approches se complètent parfaitement, si bien qu'elles s
-                                        ont souvent utilisées en parallèle. En effet, si vous appliquez ces deux
-                                        principes, votre équipe commerciale dispose d’une liste des comptes cibles (ABM)
-                                        et attire de nouveaux leads (Inbound Marketing) pour la compléter
-                                    </p>
-                                    <p>
-                                        De nos jours, les clients n’apprécient plus les méthodes de vente qu'ils jugent
-                                        trop directes. C'est pourquoi il est essentiel que votre marketing
-                                        (account-based marketing inclus) propose une réelle valeur ajoutée et ne pousse
-                                        pas le produit vers le consommateur.
-                                    </p>
-                                    <p>
-                                        Les techniques de personnalisation des campagnes marketing sont donc
-                                        essentielles pour donner envie au client de vous connaître et de découvrir votre
-                                        produit/service.
-                                    </p>
-                                </div>
+                                <h3>L'inbound marketing et l'account-based marketing sont au premier abord quelque
+                                    peu différents :</h3>
+                                <ul>
+                                    <li>
+                                        L'<strong>inbound marketing</strong> a pour objectif de créer du contenu de qualité pour
+                                        attirer des prospects qualifiés.
+                                    </li>
+                                    <li>
+                                        L'<strong>accound-based marketing</strong>, lui, met l'accent sur les prospects ou comptes
+                                        existants de manière individuelle.
+                                    </li>
+                                </ul>
+                                <p>
+                                    Toutefois, ces deux approches se complètent parfaitement, si bien qu'elles s
+                                    ont souvent utilisées en parallèle. En effet, si vous appliquez ces deux
+                                    principes, votre équipe commerciale dispose d’une liste des comptes cibles (ABM)
+                                    et attire de nouveaux leads (Inbound Marketing) pour la compléter
+                                </p>
+                                <p>
+                                    De nos jours, les clients n’apprécient plus les méthodes de vente qu'ils jugent
+                                    trop directes. C'est pourquoi il est essentiel que votre marketing
+                                    (account-based marketing inclus) propose une réelle valeur ajoutée et ne pousse
+                                    pas le produit vers le consommateur.
+                                </p>
+                                <p>
+                                    Les techniques de personnalisation des campagnes marketing sont donc
+                                    essentielles pour donner envie au client de vous connaître et de découvrir votre
+                                    produit/service.
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -187,6 +185,126 @@ export const AmbPageTemplate = ({
                 </div>
             </div>
         </section>
+        <section className="section section--gradient grey-bg p-0">
+            <div className="container">
+                <div className="section">
+                    <div className="columns">
+                        <div className="column">
+                            <div className="content">
+                                <div className="tile">
+                                    <h2 className="title has-text-white-ter has-text-centered">
+                                        Pourquoi faire de l’ABM?
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="section section--gradient p-0">
+            <div className="container">
+                <div className="section">
+                    <div className="columns">
+                        <div className="column">
+                            <div className="content">
+                                <h3>Quels sont les avantages d’utiliser l’Account Based Marketing (ABM)?
+                                    peu différents :</h3>
+                                <ul>
+                                    <li>
+                                        Un meilleur retour sur investissement (ROI)
+                                    </li>
+                                </ul>
+                                <p>L'ABM se focalise sur un nombre limité de prospects au lieu d'un vaste ensemble
+                                    d'indicateurs comme avec d'autres stratégies B2B. Concentrer vos efforts marketing
+                                    sur une plus petite cible vous permet, in fine, d'obtenir un retour sur
+                                    investissement plus important et des résultats très satisfaisants, notamment en
+                                    termes de conversion.</p>
+                                <ul>
+                                    <li>
+                                        Une approche personnalisée:
+                                    </li>
+                                </ul>
+                                <p>
+                                    Grâce à vos différentes études vous connaissez les problématiques et de votre
+                                    possible et vous pouvez donc y répondre.
+                                </p>
+                                <ul>
+                                    <li>
+                                        L’alignement des équipes commerciales et marketing
+                                    </li>
+                                </ul>
+                                <p>
+                                    Tout comme dans l’Inbound Marketing, organiser son travail de façon optimisé et
+                                    cohérente entre ces deux services est essentiel pour convertir les prospects.
+                                    Marketing et sales collaborent donc à chaque étape du processus, de l'identification
+                                    de la cible à la construction du discours commercial.
+                                </p>
+                                <ul>
+                                    <li>
+                                        Gain de temps et d’argent
+                                    </li>
+                                </ul>
+                                <p>
+                                    Grâce à ces techniques, vous utilisez votre temps efficacement en touchant
+                                    directement votre cible (finiles campagnes marketing de masse).Vos ressources sont
+                                    économisées, alors que l’efficacité des campagnes marketing est améliorée. Le but
+                                    de l’ABMest d’augmenter les recettes en utilisant le moins de ressources possible.
+                                </p>
+                                <h3>Notre approche :</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="columns">
+                        <div className="column is-one-third ">
+                            <table className="table is-bordered">
+                                <thead>
+                                <tr><th>Etape 1 : Etude </th></tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>
+                                        Identifier vos cibles et leurs besoins<br/>
+                                        Étudier vos comptes clients
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="column is-one-third has-text-centered">
+                            <table className="table is-bordered">
+                                <thead>
+                                <tr><th>Etape 2 : Plan d’action </th></tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>
+                                        Choisir les canaux de communication<br/>
+                                        Créer du contenu<br/>
+                                        Lancer les campagnes
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="column is-one-third has-text-centered">
+                            <table className="table is-bordered">
+                                <thead>
+                                <tr><th>Etude 3 : Suivi</th></tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>
+                                        Évaluer les résultats
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 );
 
@@ -198,6 +316,7 @@ const AmbPage = ({data}) => {
             title,
             heading,
             subheading,
+            button,
             inner_image,
         }
     } = data.markdownRemark;
@@ -208,6 +327,7 @@ const AmbPage = ({data}) => {
                 title={title}
                 heading={heading}
                 subheading={subheading}
+                button={button}
                 inner_image={inner_image}
             />
         </Layout>
@@ -244,6 +364,10 @@ export const pageQuery = graphql`
                             ...GatsbyImageSharpFluid
                         }
                     }
+                }
+                button {
+                    text
+                    url
                 }
             }
         }
